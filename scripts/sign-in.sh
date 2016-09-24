fdf82bf9e1af5cdb8898d29eb19b26168f341dc5
cdelsoin <cdelsoin@gmail.com>
2016-09-24 16:26:28 -0400

    updated scripts for testing
    created user cliff

--- scripts/sign-in.sh
+++ scripts/sign-in.sh
@@ -4,7 +4,9 @@ curl --include --request POST http://localhost:3000/sign-in \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "an@example.email",
-      "password": "an example password"
+      "email": "cliff",
+      "password": "cliff"
     }
   }'
+
+token=BAhJIiVmYjQ0YjcyYjM1Zjk3ZDQ2NmNmYzdkYWQ3YmFkM2RhMwY6BkVG--061f2252020e607299c7fec42b20a4c30deb5449
